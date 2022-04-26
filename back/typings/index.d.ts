445a3332b833550edc846b38d1436288a74c248c
natchy <wmainvielle@gmail.com>
2022-04-27 01:14:18 +0200

    add typings

---
+++ back/typings/index.d.ts
@@ -0,0 +1,34 @@
+export interface Account {
+  id: number
+  pseudo: string
+  discriminator: string
+  password: string
+  avatar: string | null
+}
+
+export interface Desk {
+  id: number
+  background: string | null
+  theme: string
+  color: string | null
+  accountId: number
+}
+
+export interface Folder {
+  id: number
+  name: string
+  icon: string
+  favorite: boolean
+  accountId: number
+  folderId: number | null
+}
+
+export interface File {
+  id: number
+  name: string
+  type: string
+  content: string | null
+  favorite: boolean
+  accountId: number
+  folderId: number | null
+}

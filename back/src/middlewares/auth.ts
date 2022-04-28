faede83fe8e801fa626b11dcdb574f4c5f10cddc
natchy <wmainvielle@gmail.com>
2022-04-28 10:49:37 +0200

    add auth system

--- back/src/middlewares/auth.ts
+++ back/src/middlewares/auth.ts
@@ -6,8 +6,14 @@ import APIError from '../errors/APIError';
 
 export default (req: Request, _res: Response, next: NextFunction) => {
   const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
-  if (!token) throw new APIError('this route require auth token', 403);
+  if (!token) throw new APIError('auth is failed', 401);
 
-  jwt.verify(token, <string>process.env.JWT_SECRET);
+  const decoded = jwt.verify(token, <string>process.env.JWT_SECRET);
+
+  if (typeof decoded !== 'object') throw new APIError('auth is failed', 401);
+  if (typeof decoded.id !== 'number') throw new APIError('auth is failed', 401);
+  if (typeof decoded.ms !== 'number') throw new APIError('auth is failed', 401);
+  if (req.params.userId && req.params.userId !== `${decoded.id}`) throw new APIError('id does not match with token', 401);
+
   next();
 };

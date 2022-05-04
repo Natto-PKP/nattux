d05e5203e35255d7077f73c1f5c00821ad3d08b5
natchy <wmainvielle@gmail.com>
2022-05-04 14:38:09 +0200

    front started*

---
+++ front/docs/journal.md
@@ -0,0 +1,6 @@
+# JOURNAL
+
+## 4 mai 2022
+
+- Début du projet coté front
+- Mise en place de la structure react
---
+++ front/src/components/app.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react';
+
+export default function App(): ReactElement {
+  return (
+    <>
+      <header />
+      <main />
+      <footer />
+    </>
+  );
+}
